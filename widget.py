78d2e85ea5e5db78d558f86cb8206af6eec0a369
DzKInShO2 <dzkinsho2@protonmail.com>
2025-04-09 21:30:03 +0800

    Alright, I will change the layout

--- widget.py
+++ widget.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication,
     QVBoxLayout,
-    QFormLayout,
+    QHBoxLayout,
     QWidget,
     QSlider,
     QLabel
@@ -21,16 +21,6 @@ class StylizingWindow(QWidget):
     def __create_components(self):
         root_layout = QVBoxLayout()
 
-        slider_layout = QFormLayout()
-        font_size_slider = QSlider(Qt.Orientation.Horizontal)
-        font_color_slider = QSlider(Qt.Orientation.Horizontal)
-        background_color_slider = QSlider(Qt.Orientation.Horizontal)
-
-        slider_layout.addRow(QLabel("Font Size"), font_size_slider)
-        slider_layout.addRow(QLabel("Background Color"), background_color_slider)
-        slider_layout.addRow(QLabel("Font Size"), font_color_slider)
-
-        root_layout.addLayout(slider_layout)
         self.setLayout(root_layout)
 
 
